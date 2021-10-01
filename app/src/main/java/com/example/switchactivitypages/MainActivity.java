6bef6568229309c03dc72e5a10b055d1338866d2
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-01 21:04:56 +0530

    Add code for activity one.

--- app/src/main/java/com/example/switchactivitypages/MainActivity.java
+++ app/src/main/java/com/example/switchactivitypages/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.switchactivitypages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Second.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
