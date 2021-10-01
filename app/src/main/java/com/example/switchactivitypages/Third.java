78ed7f55c5645d57f34b7d8bcf2c7f824e824a51
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-01 21:11:40 +0530

    Add code for activity third.

--- app/src/main/java/com/example/switchactivitypages/Third.java
+++ app/src/main/java/com/example/switchactivitypages/Third.java
@@ -2,13 +2,25 @@ package com.example.switchactivitypages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Third extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        b1 = findViewById(R.id.button3);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Third.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
